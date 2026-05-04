cb56cab61d3926a4d2d4652b6b40d3bc2ae633c7
Ankit Kumar <ankitks6688@gmail.com>
2026-05-04 22:06:26 +0530

    MajorityElements

--- src/arrayEasy/MajorityElementN3.java
+++ src/arrayEasy/MajorityElementN3.java
@@ -44,8 +44,42 @@ public class MajorityElementN3 {
 		*/
 		
 		//3. OPTIMAL APPROACH (MOORE VOTING ALGORITHM)
+		
 		int count1 = 0, count2 = 0; 
 		int candidate1 = 0, candidate2 = 1; 
+		for(int num : arr) {
+			if(num == candidate1) {
+				count1++; 
+			}
+			else if(num == candidate2) {
+				count2++; 
+			}
+			else if(count1 == 0) {
+				candidate1 = num; 
+				count1 = 1; 
+			}
+			else if(count2 == 0) {
+				candidate2 = num;
+				count2 = 1; 
+			}
+			else {
+				count1 --; 
+				count2-- ; 
+			}
+		}
+		count1 = 0; 
+		count2 = 0; 
+		
+		for(int num : arr) {
+			if(num == candidate1) count1++;
+			else if(num == candidate2) count2++; 
+		}
+		List<Integer> result = new ArrayList<>();
+		
+		if(count1 > n/3) result.add(candidate1);
+		if(count2 > n/3) result.add(candidate2);
+		
+		System.out.println(result);
 	}
 
 }
