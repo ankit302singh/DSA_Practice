930dd25c527d7db40cc8902b88ac34770cc1b2f6
Ankit Kumar <ankitks6688@gmail.com>
2026-05-28 15:44:00 +0530

    Merge_SubIntervals

--- src/arrayEasy/Merge_Overlapping_SubIntervals.java
+++ src/arrayEasy/Merge_Overlapping_SubIntervals.java
@@ -2,6 +2,7 @@ package arrayEasy;
 import java.util.*;
 
 public class Merge_Overlapping_SubIntervals {
+	/*
 	// ==== BETER WAY =====
 	public static void main(String[] args) {
 		int[][] intervals = {
@@ -35,6 +36,36 @@ public class Merge_Overlapping_SubIntervals {
 		}
 		for(int[] arr : result) {
 			System.out.println(arr[0] + " "+ arr[1]);
+		}
+	}
+	*/
+	
+	// ===============Optimal Approach ========
+	public static void main(String[] args) {
+		int[][] intervals = {
+				{1,3},
+				{2,6},
+				{8,10},
+				{15,18}
+		};
+		// sort intervals 
+		Arrays.sort(intervals,(a,b) -> Integer.compare(a[0], b[0]));
+		List<int[]> merged = new ArrayList<>();
+		
+		for(int[] interval : intervals) {
+			
+			// If list is empty Or no overlap 
+			if(merged.isEmpty() || merged.get(merged.size()-1)[1]<interval[0]) {
+				merged.add(interval);
+			}
+			else {
+				// merge intervals 
+				merged.get(merged.size()-1)[1] = Math.max(merged.get(merged.size()-1)[1],interval[1]);
+			}
+			
+		}
+		for(int[] arr : merged) {
+			System.out.println(arr[0]+ " "+arr[1]);
 		}
 	}
 
