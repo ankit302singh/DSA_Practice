badf44ae1717fdc3978ec12d88c4f56e1fe415c2
Ankit Kumar <ankitks6688@gmail.com>
2026-06-04 17:57:30 +0530

    merge two sorted array

--- src/arrayEasy/merge_two_sorted_Array_without_extraspace.java
+++ src/arrayEasy/merge_two_sorted_Array_without_extraspace.java
@@ -1,7 +1,11 @@
 package arrayEasy;
 import java.util.*;
+
 // ======= Better Approach (Swap + Sort ) ========
+
 public class merge_two_sorted_Array_without_extraspace {
+	
+	/*
 	public static void merge(int[] arr1, int[]arr2, int n, int m) {
 		int left = n-1; 
 		int right = 0;
@@ -31,5 +35,62 @@ public class merge_two_sorted_Array_without_extraspace {
 		System.out.println(Arrays.toString(arr1));
 		System.out.println(Arrays.toString(arr2));
 	}
+}
+  */
 
+// ===== Optimal Approach ( Gap Method / Shell Sort method ) 
+     public static int nextGap(int gap) {
+    	 if(gap <= 1)
+    		 return 0 ;
+    	 
+    	 return (gap / 2) + (gap % 2);
+     }
+     public static void merge(int[] arr1, int[] arr2, int n, int m) {
+    	 int len = n + m; 
+    	 int gap = nextGap(len);
+    	 
+    	 while(gap > 0) {
+    		 int left = 0; 
+    		 int right = left + gap; 
+    		 
+    		 while(right < len) {
+    			 // both in arr1 
+    			 if(left < n && right < n) {
+    				 if(arr1[left] > arr1[right]) {
+    					 int temp = arr1[left];
+    					 arr1[left] =   arr1[right];
+    					 arr1[right] = temp; 
+    				 }
+    			 }
+    			 // left in arr1, right in arr2 
+    			 else if(left < n && right >= n) {
+    				 if(arr1[left] > arr2[right - n]) {
+    					 int temp = arr1[left];
+    					 arr1[left] = arr2[right - n];
+    					 arr2[right - n] = temp;
+    				 }
+    			 }
+    			 // both in arr2 
+    			 else {
+    				 if(arr2[left - n] > arr2[right - n]) {
+    					 int temp = arr2[left - n];
+    					 arr2[left - n] = arr2[right - n];
+    					 arr2[right - n] = temp ; 
+    				 }
+    			 }
+    			 left++; 
+    			 right++; 
+    		 }
+    		 gap = nextGap(gap);
+    	 }
+     }
+     public static void main(String[] args) {
+    	 int[] arr1 = {1,4,8,10};
+    	 int[] arr2 = {2,3,9};
+    	 
+    	 merge(arr1, arr2, arr1.length, arr2.length);
+    	 
+    	 System.out.println(Arrays.toString(arr1));
+    	 System.out.println(Arrays.toString(arr2));
+     }
 }
