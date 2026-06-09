a9d9dc7e1934262a6d789b26bf2ee89640b19b00
Ankit Kumar <ankitks6688@gmail.com>
2026-06-09 09:30:19 +0530

    cont inversion

--- src/arrayEasy/Count_Inversion_an_Array.java
+++ src/arrayEasy/Count_Inversion_an_Array.java
@@ -1,6 +1,8 @@
 package arrayEasy;
 
 public class Count_Inversion_an_Array {
+	
+	/*
 	public static void main(String[] args) {
 		int[] arr = {5,3,2,4,1};
 		int count = 0; 
@@ -12,6 +14,51 @@ public class Count_Inversion_an_Array {
 			}
 		}
 		System.out.println("Inversion Count = "+ count);
+	}
+	*/
+	public static void main(String[] args) {
+		int[] arr = {5,3,2,4,1};
+		int inversions = mergeSort(arr, 0, arr.length-1);
+		System.out.println("Inversion Count = " + inversions);
+	}
+	static int mergeSort(int[] arr, int low, int high) {
+		int count = 0; 
+		if(low >= high) {
+			return count ;
+		}
+		int mid = (low + high)/2;
+		count += mergeSort(arr, low, mid);
+		count += mergeSort(arr, mid+1, high);
+		count += merge(arr, low, mid, high);
+		return count;
+	}
+	static int merge(int[] arr, int low, int mid, int high) {
+		int count = 0; 
+		int[] temp = new int[high - low +1];
+		
+		int left = low; 
+		int right = mid + 1; 
+		int k = 0; 
+		
+		while(left <= mid && right <= high) {
+			if(arr[left] <= arr[right]) {
+				temp[k++] = arr[left++];
+			}
+			else {
+				count += (mid - left +1);
+				temp[k++] = arr[right++];
+			}
+		}
+		while(left <= mid) {
+			temp[k++] = arr[left++];
+		}
+		while(right <= high) {
+			temp[k++] = arr[right++];
+		}
+		for(int i = low; i<= high; i++) {
+			arr[i] = temp[i-low];
+		}
+		return count;
 	}
 
 }
