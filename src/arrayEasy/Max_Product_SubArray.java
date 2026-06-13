0bb78a76e64aca1da5b486ecfc8dfd6774d18295
Ankit Kumar <ankitks6688@gmail.com>
2026-06-13 12:20:15 +0530

    maxProduct

--- src/arrayEasy/Max_Product_SubArray.java
+++ src/arrayEasy/Max_Product_SubArray.java
@@ -1,6 +1,8 @@
 package arrayEasy;
 
 public class Max_Product_SubArray {
+	/*
+// ======== Approach 1st ==============
 	public static int maxProduct(int[] arr) {
 		int maxProduct = Integer.MIN_VALUE;
 		
@@ -19,5 +21,30 @@ public class Max_Product_SubArray {
 		int[] arr = {2,3,-2,4}; 
 		System.out.println("MaxProduct = "+ maxProduct(arr));
 	}
-
+      */
+	
+// ===== optimal approach ===========
+	public static int maxProduct(int[] arr) {
+		int maxEnding = arr[0];
+		int minEnding = arr[0];
+		int answer = arr[0];
+		
+		for(int i=1; i<arr.length; i++) {
+			int current = arr[i];
+			
+			if(current < 0) {
+				int temp = maxEnding; 
+				maxEnding = minEnding; 
+				minEnding = temp ; 
+			}
+			maxEnding = Math.max(current, maxEnding * current);
+			minEnding = Math.min(current, minEnding * current);
+			answer = Math.max(answer, maxEnding);
+		}
+		return answer; 
+	}
+	public static void main(String[] args) {
+		int[] arr = {2,3,-2,4}; 
+		System.out.println("MaxProduct = "+ maxProduct(arr));
+	}
 }
