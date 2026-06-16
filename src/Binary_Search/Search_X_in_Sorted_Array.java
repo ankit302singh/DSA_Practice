9ad18ee6d847200f8198a468f843765225afbe96
Ankit Kumar <ankitks6688@gmail.com>
2026-06-16 16:47:40 +0530

    Binary_Search

--- src/Binary_Search/Search_X_in_Sorted_Array.java
+++ src/Binary_Search/Search_X_in_Sorted_Array.java
@@ -1,6 +1,9 @@
 package Binary_Search;
+import java.util.*;
 
 public class Search_X_in_Sorted_Array {
+// ======Normal Way(Liner Search) ===========
+	/*
 	public static int search(int[] arr, int x) {
 		for(int i=0; i<arr.length; i++) {
 			if(arr[i] == x) {
@@ -16,5 +19,15 @@ public class Search_X_in_Sorted_Array {
 		
 		System.out.println("Index = " + result);
 	}
-
+   */
+// ===========Better Approach ( Using Built-in Binary Search) ==========
+	 
+	public static void main(String[] args) {
+		int[] arr = {2,4,6,8,10,12}; 
+		int x = 8; 
+		
+		int index = Arrays.binarySearch(arr, x);
+		
+		System.out.println("Index = " + index);
+	}
 }
