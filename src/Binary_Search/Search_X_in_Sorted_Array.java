a626238d3f5b768c719dd57608ab23bf29e4aa4e
Ankit Kumar <ankitks6688@gmail.com>
2026-06-17 09:23:13 +0530

    Binary Search

--- src/Binary_Search/Search_X_in_Sorted_Array.java
+++ src/Binary_Search/Search_X_in_Sorted_Array.java
@@ -21,7 +21,7 @@ public class Search_X_in_Sorted_Array {
 	}
    */
 // ===========Better Approach ( Using Built-in Binary Search) ==========
-	 
+	/* 
 	public static void main(String[] args) {
 		int[] arr = {2,4,6,8,10,12}; 
 		int x = 8; 
@@ -29,5 +29,33 @@ public class Search_X_in_Sorted_Array {
 		int index = Arrays.binarySearch(arr, x);
 		
 		System.out.println("Index = " + index);
+	}
+	*/
+	
+// ====== Optimal Approach ( Manual Binary Search)==========
+	public static int binarySearch(int[] arr, int x) {
+		int low = 0; 
+		int high = arr.length -1 ; 
+		while(low <= high) {
+			int mid = low + (high - low)/2;
+			
+			if(arr[mid] == x) {
+				return mid ; 
+			}
+			else if(arr[mid] < x) {
+				low = mid + 1; 
+			}
+			else {
+				high = mid - 1; 
+			}
+		}
+		return -1; 
+	}
+	public static void main(String[] args) {
+		int[] arr = {2,4,6,8,10,12}; 
+		int x = 8; 
+		
+		int result = binarySearch(arr, x);
+		System.out.println("Indec = " + result);
 	}
 }
