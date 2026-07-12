abfc9241eef7de55955ef7e77e1053a66456ca4f
Ankit Kumar <ankitks6688@gmail.com>
2026-07-12 14:19:48 +0530

    countOccurrence

--- src/Binary_Search/Count_Occurrence.java
+++ src/Binary_Search/Count_Occurrence.java
@@ -11,5 +11,21 @@ public class Count_Occurrence {
 		}
 		return count; 
 	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter array size: ");
+		int n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		System.out.println("Enter sorted array: ");
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.print("enter element to search: ");
+		int x = sc.nextInt();
+		int ans = countOccurrence(arr,x);
+		System.out.println("Occurrence: " + ans);
+	}
 
 }
