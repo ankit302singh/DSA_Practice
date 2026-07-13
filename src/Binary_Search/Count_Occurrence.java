4b5873c45584462061ffc2e851c5b0aa54fbea31
Ankit Kumar <ankitks6688@gmail.com>
2026-07-13 21:50:18 +0530

    countOccurrence

--- src/Binary_Search/Count_Occurrence.java
+++ src/Binary_Search/Count_Occurrence.java
@@ -1,7 +1,12 @@
 package Binary_Search;
 import java.util.Scanner;
 
+// <<<<<< Normal Way (Linear Search) >>>>>>>
+
+
 public class Count_Occurrence {
+	/*
+	 * 
 	public static int countOccurrence(int[] arr, int x) {
 		int count = 0; 
 		for(int num : arr) {
@@ -27,5 +32,44 @@ public class Count_Occurrence {
 		int ans = countOccurrence(arr,x);
 		System.out.println("Occurrence: " + ans);
 	}
-
+	*/
+	
+	// <<<<<  Better Way (Binary Search + Expand Left & Right) >>>>
+        
+	public static int countOccurrence(int[] arr, int x) {
+		int low = 0; 
+		int high = arr.length - 1; 
+		int index = -1; 
+		
+		while(low <= high) {
+			int mid = low + (high - low)/2 ; 
+			
+			if(arr[mid] == x) {
+				index = mid ; 
+				break ; 
+			}
+			else if(arr[mid] < x) {
+				low = mid + 1; 
+			}
+			else {
+				high = mid - 1; 
+			}
+		}
+		if(index == -1)
+			return 0 ; 
+			
+			int count = 1; 
+			int left  = index - 1; 
+			while(left >= 0 && arr[left] == x) {
+				count++; 
+				left--; 
+			}
+			int right = index - 1; 
+			while(right < arr.length && arr[right] == x) {
+				count++; 
+				right++; 
+			}
+			return count; 
+		
+	}
 }
