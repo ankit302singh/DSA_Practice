150dd53a3f28cc87a1ec34fdb65725496da8b973
Ankit Kumar <ankitks6688@gmail.com>
2026-07-16 10:56:27 +0530

    sorted

--- src/Binary_Search/Search_in_rotated_sorted_array1.java
+++ src/Binary_Search/Search_in_rotated_sorted_array1.java
@@ -48,4 +48,19 @@ public class Search_in_rotated_sorted_array1 {
 		}
 		return low; 
 	}
+	public static int binarySearch(int[] arr, int low, int high, int target) {
+		while(low <= high) {
+			int mid = low + (high - low)/2; 
+			
+			if(arr[mid] == target)
+				return mid; 
+			
+			else if(arr[mid] < target)
+				low = mid + 1; 
+			
+			else 
+				high = mid - 1; 
+		}
+		 return -1; 
+	}
 }
