85ba4bbeef6f9c7c670bcca68c9618a3743750bb
Ankit Kumar <ankitks6688@gmail.com>
2026-07-16 10:50:26 +0530

    sorted

--- src/Binary_Search/Search_in_rotated_sorted_array1.java
+++ src/Binary_Search/Search_in_rotated_sorted_array1.java
@@ -1,7 +1,9 @@
 package Binary_Search;
 import java.util.Scanner;
 
-public class Search_in_rotated_sorted_array1 {
+public class Search_in_rotated_sorted_array1 { 
+	// <<<<< Vai Linear Search >>>>>
+	/*
 	public static int search(int[]arr, int target) {
 		for(int i=0; i< arr.length; i++) {
 			if(arr[i] == target) {
@@ -28,5 +30,22 @@ public class Search_in_rotated_sorted_array1 {
 		int ans = search(arr, target);
 		System.out.println("Target fount at: " + ans);
 	}
-
+	
+	*/
+    // <<< Better Way (find Pivot then Binary Search ) >>>
+	
+	public static int findPivot(int[] arr) {
+		int low = 0; 
+		int high = arr.length - 1; 
+		while(low < high) {
+			int mid = low + (high - low)/2; 
+			if(arr[mid] > arr[high]) {
+				low = mid + 1; 
+			}
+			else {
+				high = mid; 
+			}
+		}
+		return low; 
+	}
 }
