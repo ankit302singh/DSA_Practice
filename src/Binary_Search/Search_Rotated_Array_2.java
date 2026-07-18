8c4443c1e0a68f65ce8b0f9360ac0745e9acbbd4
Ankit Kumar <ankitks6688@gmail.com>
2026-07-18 12:38:14 +0530

    sortedarray

--- src/Binary_Search/Search_Rotated_Array_2.java
+++ src/Binary_Search/Search_Rotated_Array_2.java
@@ -11,6 +11,26 @@ public class Search_Rotated_Array_2 {
 		}
 		return false;
 	}
-	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter size of array: ");
+		int n = sc.nextInt();
+		
+		int arr[] = new int[n];
+		System.out.println("Enter array Element: ");
+		
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.print("Enter target: ");
+		int target = sc.nextInt();
+		
+		boolean ans = search(arr, target);
+		
+		System.out.println(ans);
+		
+		sc.close();
+	}
 
 }
