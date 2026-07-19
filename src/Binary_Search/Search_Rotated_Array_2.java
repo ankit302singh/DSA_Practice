878e3abc9d8c4607357b52c6d0e43fb85c5bc2a3
Ankit Kumar <ankitks6688@gmail.com>
2026-07-19 12:32:19 +0530

    sortedArray

--- src/Binary_Search/Search_Rotated_Array_2.java
+++ src/Binary_Search/Search_Rotated_Array_2.java
@@ -59,8 +59,30 @@ public class Search_Rotated_Array_2 {
 					low = mid + 1; 
 				}
 			}
+			else {
+				if(target > arr[mid] && target <= arr[high]) {
+					low = mid + 1; 
+				}
+				else {
+					high = mid - 1; 
+				}
+			}
 		}
 		return false; 
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter size: ");
+		int n = sc.nextInt();
+		
+		int arr[] = new int[n];
+		
+		System.out.println("Enter element: ");
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
 	}
 
 }
