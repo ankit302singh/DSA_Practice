73d9e92522815d8b05ed3c06f3fc31f5ccc24466
Ankit Kumar <ankitks6688@gmail.com>
2026-07-20 12:31:54 +0530

    sorted

--- src/Binary_Search/Minimun_in_Sorted_Array.java
+++ src/Binary_Search/Minimun_in_Sorted_Array.java
@@ -44,8 +44,25 @@ public class Minimun_in_Sorted_Array {
 				ans = Math.min(ans, arr[mid]);
 				low = mid + 1; 
 			}
+			else {
+				ans = Math.min(ans, arr[mid]);
+				high = mid - 1; 
+			}
 		}
 		return ans; 
+	}
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter size: ");
+		int n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		
+		System.out.println("Enter Element: ");
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
 	}
 
 }
