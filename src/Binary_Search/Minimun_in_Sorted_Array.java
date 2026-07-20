39a6e7ba18ce1dd6c2e9f7d4cba7d019d94e3754
Ankit Kumar <ankitks6688@gmail.com>
2026-07-20 11:21:59 +0530

    mininumarray

---
+++ src/Binary_Search/Minimun_in_Sorted_Array.java
@@ -0,0 +1,15 @@
+package Binary_Search;
+
+public class Minimun_in_Sorted_Array {
+	public static int findMin(int[] arr) {
+		int min = arr[0];
+		
+		for(int i = 1; i<arr.length; i++) {
+			if(arr[i] < min) {
+				min = arr[i];
+			}
+		}
+		return min; 
+	}
+
+}
