b2c6eedb46ef48f5db835af2c334f0eb60441b75
Ankit Kumar <ankitks6688@gmail.com>
2026-07-21 12:31:17 +0530

    rotated array

--- src/Binary_Search/How_Many_Times_Array_Rotated.java
+++ src/Binary_Search/How_Many_Times_Array_Rotated.java
@@ -1,4 +1,5 @@
 package Binary_Search;
+import java.util.Scanner; 
 
 public class How_Many_Times_Array_Rotated {
 	public static int countRotations(int[] arr) {
@@ -11,6 +12,19 @@ public class How_Many_Times_Array_Rotated {
 			}
 		}
 		return index; 
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter size: ");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		System.out.println("Enter array: ");
+		
+		for(int i = 0; i< n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println("Array is rotated: " + countRotations(arr) + " times.");
 	}
 
 }
