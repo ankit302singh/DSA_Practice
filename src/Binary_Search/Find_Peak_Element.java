ae2a3932900c12bee873cde369333f9c5d3fceae
Ankit Kumar <ankitks6688@gmail.com>
2026-07-26 01:13:29 +0530

    peak

--- src/Binary_Search/Find_Peak_Element.java
+++ src/Binary_Search/Find_Peak_Element.java
@@ -80,6 +80,15 @@ public class Find_Peak_Element {
 	// <<<< Optimal way (binary Search) 
 	public static int findPeak(int[] arr) {
 		int low = 0; 
+		int high = arr.length - 1; 
+		
+		while(low < high) {
+			int mid = low + (high - low)/2; 
+			
+			if(arr[mid] < arr[mid + 1]) {
+				low = mid + 1; 
+			}
+		}
 		return low; 
 	}
 
