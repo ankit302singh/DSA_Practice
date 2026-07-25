e69ed5faac6e8b4bed7f552b00ac99671da37903
Ankit Kumar <ankitks6688@gmail.com>
2026-07-25 10:53:05 +0530

    peak

--- src/Binary_Search/Find_Peak_Element.java
+++ src/Binary_Search/Find_Peak_Element.java
@@ -45,6 +45,11 @@ public class Find_Peak_Element {
 		int n = arr.length; 
 		for(int i=0; i<n; i++) {
 			
+			boolean left = (i==0) || arr[i] >= arr[i-1];
+			boolean right = (i == n-1) || arr[i] >= arr[i+1];
+			
+			if(left && right)
+				return i; 
 		}
 		return -1; 
 	}
