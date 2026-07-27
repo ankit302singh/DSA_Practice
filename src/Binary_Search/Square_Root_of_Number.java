5d0058c2ec9a7cb90d1ab405cff6b570594fe1fd
Ankit Kumar <ankitks6688@gmail.com>
2026-07-27 15:02:33 +0530

    square

--- src/Binary_Search/Square_Root_of_Number.java
+++ src/Binary_Search/Square_Root_of_Number.java
@@ -5,7 +5,12 @@ public class Square_Root_of_Number {
      public static int squareRoot(int n) {
     	 int ans = 0; 
     	 for(int i=1; i<=n; i++) {
-    		 
+    		 if((long)i * i <= n) {
+    			 ans = i; 
+    		 }
+    		 else {
+    			 break; 
+    		 }
     	 }
     	 return ans; 
      }
