505e77899c6b2a06e41313c60e2f107b9e4d4a99
Ankit Kumar <ankitks6688@gmail.com>
2026-07-27 15:01:09 +0530

    Square

---
+++ src/Binary_Search/Square_Root_of_Number.java
@@ -0,0 +1,12 @@
+package Binary_Search;
+import java.util.Scanner;
+
+public class Square_Root_of_Number {
+     public static int squareRoot(int n) {
+    	 int ans = 0; 
+    	 for(int i=1; i<=n; i++) {
+    		 
+    	 }
+    	 return ans; 
+     }
+}
