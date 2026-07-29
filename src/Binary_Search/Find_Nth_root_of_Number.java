ce3cd6a3ca823fe1af5d4a8f74c19f06e02233cf
Ankit Kumar <ankitks6688@gmail.com>
2026-07-29 20:55:50 +0530

    power

--- src/Binary_Search/Find_Nth_root_of_Number.java
+++ src/Binary_Search/Find_Nth_root_of_Number.java
@@ -2,7 +2,11 @@ package Binary_Search;
 
 public class Find_Nth_root_of_Number {
 	public static long power(int base, int exponent) {
+		long result = 1; 
 		
+		for(int i = 1; i <= exponent; i++) {
+			result *= base; 
+		}
 		return - 1; 
 	}
 
