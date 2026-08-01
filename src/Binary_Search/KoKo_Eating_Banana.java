ad39f55397f47c8b74be1c8879bce51daa0000ce
Ankit Kumar <ankitks6688@gmail.com>
2026-08-01 11:20:35 +0530

    earting

--- src/Binary_Search/KoKo_Eating_Banana.java
+++ src/Binary_Search/KoKo_Eating_Banana.java
@@ -2,6 +2,11 @@ package Binary_Search;
 
 public class KoKo_Eating_Banana {
      public static int minSpeed(int[] piles, int k) {
+    	 int max = piles[0];
+    	 for(int pile : piles) {
+    		 if(pile > max)
+    			 max = pile; 
+    	 }
     	 return -1; 
      }
 }
