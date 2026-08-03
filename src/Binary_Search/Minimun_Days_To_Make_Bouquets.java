c6d6e5f35253425cdd5d273c69898ddb335e4922
Ankit Kumar <ankitks6688@gmail.com>
2026-08-03 12:49:12 +0530

    Bouquets

---
+++ src/Binary_Search/Minimun_Days_To_Make_Bouquets.java
@@ -0,0 +1,9 @@
+package Binary_Search;
+
+public class Minimun_Days_To_Make_Bouquets {
+	static int countBouquets(int[] bloomDay, int day, int k) {
+		int bouquets = 0; 
+		return bouquets; 
+	}
+
+}
