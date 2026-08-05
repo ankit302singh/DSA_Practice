ce9792f4e65d6bfdc9df63a222069c9234da7515
Ankit Kumar <ankitks6688@gmail.com>
2026-08-05 21:18:39 +0530

    smalles

--- src/Binary_Search/Find_the_Smallest_Divsior_Given_Tharshold.java
+++ src/Binary_Search/Find_the_Smallest_Divsior_Given_Tharshold.java
@@ -3,6 +3,11 @@ package Binary_Search;
 public class Find_the_Smallest_Divsior_Given_Tharshold {
      public static int smallestDivisor(int[] nums, int threshold) {
     	 
+    	 int max = nums[0];
+    	 for(int num : nums) {
+    		 if(num > max)
+    			 max = num ; 
+    	 }
     	 return -1; 
      }
 }
