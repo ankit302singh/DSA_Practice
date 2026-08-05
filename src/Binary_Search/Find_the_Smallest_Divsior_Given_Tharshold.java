f02492c8c5aa44bacebbb994192a4d64b8197e70
Ankit Kumar <ankitks6688@gmail.com>
2026-08-05 21:19:59 +0530

    smalles

--- src/Binary_Search/Find_the_Smallest_Divsior_Given_Tharshold.java
+++ src/Binary_Search/Find_the_Smallest_Divsior_Given_Tharshold.java
@@ -8,6 +8,14 @@ public class Find_the_Smallest_Divsior_Given_Tharshold {
     		 if(num > max)
     			 max = num ; 
     	 }
+    	 
+    	 for(int divisor = 1; divisor <= max; divisor++) {
+    		 int sum = 0; 
+    		 
+    		 for(int num : nums) {
+    			 
+    		 }
+    	 }
     	 return -1; 
      }
 }
