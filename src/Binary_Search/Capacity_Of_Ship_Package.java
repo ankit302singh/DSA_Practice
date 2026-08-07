bb52ac2bd8dbaf082fdf4e874cfd60425208697e
Ankit Kumar <ankitks6688@gmail.com>
2026-08-07 13:04:50 +0530

    ship

--- src/Binary_Search/Capacity_Of_Ship_Package.java
+++ src/Binary_Search/Capacity_Of_Ship_Package.java
@@ -3,6 +3,11 @@ package Binary_Search;
 public class Capacity_Of_Ship_Package {
 	static int findDays(int[] weights, int capacity) {
 		int days = 1; 
+		int load = 0; 
+		
+		for(int weight : weights) {
+			
+		}
 		return days; 
 	}
 
