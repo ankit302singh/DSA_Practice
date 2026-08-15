35117496e9c7a58ba55207ef51d333c1b73dc290
Ankit Kumar <ankitks6688@gmail.com>
2026-08-15 21:26:57 +0530

    Books

--- src/Binary_Search/Book_Allocation_Problem.java
+++ src/Binary_Search/Book_Allocation_Problem.java
@@ -3,7 +3,10 @@ import java.util.*;
 
 public class Book_Allocation_Problem {
 	static int findMinimun(int[] books, int students) {
-		
+		if(students > books.length) {
+			return -1; 
+		}
+		return solve(books, 0, students);
 	}
 
 }
