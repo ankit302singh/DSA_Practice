6c2463d0fbbc6b27c111c3eb1cb31eb128b01e4b
Ankit Kumar <ankitks6688@gmail.com>
2026-08-15 21:28:31 +0530

    Books

--- src/Binary_Search/Book_Allocation_Problem.java
+++ src/Binary_Search/Book_Allocation_Problem.java
@@ -8,5 +8,11 @@ public class Book_Allocation_Problem {
 		}
 		return solve(books, 0, students);
 	}
+	static int solve(int[] books, int index, int students) {
+		
+		if(students == 1) {
+			int sum = 0; 
+		}
+	}
 
 }
