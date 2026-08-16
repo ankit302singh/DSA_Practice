cde2a957c9c69bb3212985455cde91d15033e433
Ankit Kumar <ankitks6688@gmail.com>
2026-08-16 12:31:08 +0530

    books

--- src/Binary_Search/Book_Allocation_Problem.java
+++ src/Binary_Search/Book_Allocation_Problem.java
@@ -2,6 +2,7 @@ package Binary_Search;
 import java.util.*;
 
 public class Book_Allocation_Problem {
+	/*
 	static int findMinimum(int[] books, int students) {
 		if(students > books.length) {
 			return -1; 
@@ -38,5 +39,12 @@ public class Book_Allocation_Problem {
 		
 		System.out.println(findMinimum(books, students));
 	}
-
+	*/
+	
+	// <<< Optimal Way (Binary Search) >>> 
+	
+	static boolean canAllocate(int[] books, int students, int limit) {
+		
+		int studentCount = 1; 
+	}
 }
