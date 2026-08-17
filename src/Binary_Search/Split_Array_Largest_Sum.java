b1877fb9f5767be3e14a029b18c2ee1a1559ecce
Ankit Kumar <ankitks6688@gmail.com>
2026-08-17 17:40:59 +0530

    split

--- src/Binary_Search/Split_Array_Largest_Sum.java
+++ src/Binary_Search/Split_Array_Largest_Sum.java
@@ -3,6 +3,12 @@ package Binary_Search;
 public class Split_Array_Largest_Sum {
 	static boolean canSplit(int[] arr, int k, int maxSum) {
 		
+		int subarrays = 0; 
+		int currentSum = 0; 
+		
+		for(int num : arr) {
+			
+		}
 	}
    
 }
