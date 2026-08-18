31764821837febc9d0536dccc13e3563c2df2f74
Ankit Kumar <ankitks6688@gmail.com>
2026-08-18 10:56:55 +0530

    split

--- src/Binary_Search/Split_Array_Largest_Sum.java
+++ src/Binary_Search/Split_Array_Largest_Sum.java
@@ -38,6 +38,15 @@ public class Split_Array_Largest_Sum {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int[] arr = new int[n];
+		
+		for(int i = 0 ; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int k = sc.nextInt();
+		int answer = splitArray(arr, k);
+		System.out.println(answer);
+		
+		sc.close();
 	}
    
 }
