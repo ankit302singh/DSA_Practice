ab4cc4db8beb55a8708a52c40b51399cf08d691f
Ankit Kumar <ankitks6688@gmail.com>
2026-08-19 17:08:39 +0530

    painter

--- src/Binary_Search/Painters_Partition.java
+++ src/Binary_Search/Painters_Partition.java
@@ -1,5 +1,11 @@
 package Binary_Search;
 
 public class Painters_Partition {
+	
+	static boolean canPaint(int[] boards, int k , int limit ) {
+		
+		int painters = 1; 
+		int currentSum = 0; 
+	}
 
 }
