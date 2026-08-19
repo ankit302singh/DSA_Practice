8b5492373df02c1befbf35c5afa9fe73b152ab9e
Ankit Kumar <ankitks6688@gmail.com>
2026-08-20 00:08:18 +0530

    painters

--- src/Binary_Search/Painters_Partition.java
+++ src/Binary_Search/Painters_Partition.java
@@ -50,7 +50,24 @@ public class Painters_Partition {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("Enter number of boards: ");
+		int n = sc.nextInt();
 		
+		int[] boards = new int[n];
+		
+		System.out.println("Enter board Sizes: ");
+		
+		for(int i =0; i<n; i++) {
+			boards[i] = sc.nextInt();
+		}
+		System.out.println("Enter number of painters: ");
+		int k = sc.nextInt();
+		
+		int answer = painterPartition(boards, k);
+		
+		System.out.println(answer);
+		
+		sc.close();
 	}
 
 }
