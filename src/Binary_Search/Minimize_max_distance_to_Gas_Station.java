87ec21ad63221d1b898862835852193087f24530
Ankit Kumar <ankitks6688@gmail.com>
2026-08-20 17:47:07 +0530

    maximize

--- src/Binary_Search/Minimize_max_distance_to_Gas_Station.java
+++ src/Binary_Search/Minimize_max_distance_to_Gas_Station.java
@@ -1,5 +1,11 @@
 package Binary_Search;
+import java.util.*;
 
 public class Minimize_max_distance_to_Gas_Station {
+	public static double minimizeMaxDistance(int[] arr, int k) {
+		int n = arr.length; 
+		
+		int[] placed = new int[n-1];
+	}
 
 }
