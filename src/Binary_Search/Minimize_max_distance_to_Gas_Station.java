f50b7350fa80145b258de0ca14a333f6740f814d
Ankit Kumar <ankitks6688@gmail.com>
2026-08-21 20:50:44 +0530

    javapractice

--- src/Binary_Search/Minimize_max_distance_to_Gas_Station.java
+++ src/Binary_Search/Minimize_max_distance_to_Gas_Station.java
@@ -6,6 +6,15 @@ public class Minimize_max_distance_to_Gas_Station {
 		int n = arr.length; 
 		
 		int[] placed = new int[n-1];
+		for(int station = 1; station <= k; station++) {
+			
+			double maxSection = -1; 
+			int maxIndex = -1; 
+			
+			for(int i = 0; i < n-1; i++) {
+				double originealGap = arr[i+1] - arr[i];
+			}
+		}
 	}
 
 }
