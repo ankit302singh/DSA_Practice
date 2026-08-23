5616e3e65f474d59ce5442f1377c5fbe175be43f
Ankit Kumar <ankitks6688@gmail.com>
2026-08-23 10:52:22 +0530

    jaa

--- src/Binary_Search/Find_row_with_maximum_1s.java
+++ src/Binary_Search/Find_row_with_maximum_1s.java
@@ -20,5 +20,12 @@ public class Find_row_with_maximum_1s {
 		}
 		return rowIndex; 
 	}
+	public static void main(String[] args) {
+		int[][] matrix = {{0,0,0,1,1},
+				{0,0,1,1,1},
+				{0,0,0,0,1},
+				{0,1,1,1,1}
+		};
+	}
 
 }
