db3918fd67dd1deab2fabacd4f40d5911fca1540
Ankit Kumar <ankitks6688@gmail.com>
2026-09-02 10:58:51 +0530

    java

--- src/Binary_Search/Search_In_2D_Matrix.java
+++ src/Binary_Search/Search_In_2D_Matrix.java
@@ -35,11 +35,12 @@ public class Search_In_2D_Matrix {
 				matrix[i][j] = sc.nextInt();
 			}
 		}
-		System.out.println("Enter target: ");
+		System.out.print("Enter target: ");
 		int target = sc.nextInt();
 		
 		boolean result = searchMatrix(matrix, target);
 		
-		
+		System.out.println("Target found: " + result);
+		sc.close();
 	}
 }
