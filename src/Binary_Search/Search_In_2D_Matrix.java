72350fa89bd615583720b75929796664ee7504f8
Ankit Kumar <ankitks6688@gmail.com>
2026-09-02 10:45:42 +0530

    'java

--- src/Binary_Search/Search_In_2D_Matrix.java
+++ src/Binary_Search/Search_In_2D_Matrix.java
@@ -3,7 +3,11 @@ import java.util.*;
 
 public class Search_In_2D_Matrix {
 	public static boolean searcMatrix(int[][] matrix, int target) {
-		
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[i].length; j++) {
+				
+			}
+		}
 		return false; 
 	}
 
