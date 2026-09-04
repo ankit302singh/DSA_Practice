84bc9e3d470601c7f28b5b9bbd150bba52de1384
Ankit Kumar <ankitks6688@gmail.com>
2026-09-04 10:06:19 +0530

    java

--- src/Binary_Search/Search_In_2D_Matrix.java
+++ src/Binary_Search/Search_In_2D_Matrix.java
@@ -158,7 +158,10 @@ public class Search_In_2D_Matrix {
 		System.out.println("Enter matrix Element: ");
 		
 		for(int i = 0; i < rows; i++) {
-			
+			for(int j = 0; j < cols; j++) {
+				matrix[i][j] = sc.nextInt();
+			}
 		}
+		
 	}
 }
