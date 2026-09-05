69ab49bbe4e960bf135e1f82903ea47fdcfeaf32
Ankit Kumar <ankitks6688@gmail.com>
2026-09-05 13:36:36 +0530

    java

--- src/Binary_Search/Find_Peak_Element_2.java
+++ src/Binary_Search/Find_Peak_Element_2.java
@@ -5,6 +5,15 @@ public class Find_Peak_Element_2 {
 	// *** check every element ***
 	public static int[] findPeakGrid(int[][] mat) {
 		
+		int rows = mat.length; 
+		int cols = mat[0].length;
+		
+		for(int i = 0; i < rows; i++) {
+			
+			for(int j = 0; j < cols; j++) {
+				
+			}
+		}
 		return new int[] {-1,-1};
 	}
 
