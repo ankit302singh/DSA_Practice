9a860e3c6c1506ee64ddf6ae3f468edd7d11307a
Ankit Kumar <ankitks6688@gmail.com>
2026-09-06 10:44:24 +0530

    java

--- src/Binary_Search/Find_Peak_Element_2.java
+++ src/Binary_Search/Find_Peak_Element_2.java
@@ -59,6 +59,12 @@ public class Find_Peak_Element_2 {
 		int maxRow = 0; 
 		int maxCol = 0; 
 		
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < cols; j++) {
+				
+			}
+		}
+		
 		return new int[] {maxRow, maxCol};
 	}
 
