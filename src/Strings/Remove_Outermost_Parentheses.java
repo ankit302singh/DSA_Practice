fd570b18bf93912b94c3ca397400de3ebdf961a9
Ankit Kumar <ankitks6688@gmail.com>
2026-09-07 11:23:20 +0530

    java

--- src/Strings/Remove_Outermost_Parentheses.java
+++ src/Strings/Remove_Outermost_Parentheses.java
@@ -1,5 +1,10 @@
 package Strings;
 
 public class Remove_Outermost_Parentheses {
-
+// <<< Normal Way >>> 
+	public static String removeOuter(String s) {
+		
+		String result = "";
+		return result; 
+	}
 }
