0bd126ffc4e8f501d9795cb6d9fd4f2bf06b053a
Ankit Kumar <ankitks6688@gmail.com>
2026-09-09 00:19:23 +0530

    java

--- src/Strings/Remove_Outermost_Parentheses.java
+++ src/Strings/Remove_Outermost_Parentheses.java
@@ -76,20 +76,7 @@ public class Remove_Outermost_Parentheses {
 
 		int count = 0;
 		for(int i = 0; i < s.length(); i++){
-			char ch = s.charAt(i);
-			if(ch == '('){
-				if(count > 0){
-					result.append(ch);
-				}
-				count++;
-			}
-			else{
-				count--;
-				if(count > 0){
-					result.append(ch);
-				}
-			}
-		}
+			
 		return result.toString();
 	}
 
