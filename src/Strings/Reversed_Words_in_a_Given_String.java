ccbcc5251fae361bc4be868a42b3cd7b12ac832a
Ankit Kumar <ankitks6688@gmail.com>
2026-09-10 14:47:07 +0530

    java

--- src/Strings/Reversed_Words_in_a_Given_String.java
+++ src/Strings/Reversed_Words_in_a_Given_String.java
@@ -5,6 +5,13 @@ public class Reversed_Words_in_a_Given_String {
 	public static String reverseWord(String s) {
 		
 		s = s.trim();
+		String[] words = s.split("\\s+");
+		
+		StringBuilder result = new StringBuilder();
+		
+		for(int i = words.length - 1; i >= 0; i--) {
+			
+		}
 	}
 
 }
