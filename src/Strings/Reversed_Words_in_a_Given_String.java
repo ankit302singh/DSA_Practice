bc0a587fc4219e9a7c143e9ea811694b916e840d
Ankit Kumar <ankitks6688@gmail.com>
2026-09-11 11:35:27 +0530

    java

--- src/Strings/Reversed_Words_in_a_Given_String.java
+++ src/Strings/Reversed_Words_in_a_Given_String.java
@@ -43,7 +43,12 @@ public class Reversed_Words_in_a_Given_String {
 		int i = s.length() - 1; 
 		
 		while(i >= 0) {
-			
+			while(i >= 0 && s.charAt(i) == ' ') {
+				i--; 
+			}
+			if(i < 0) {
+				break; 
+			}
 		}
 	}
 
