ad2c1c330e8bdba6ff6be2690f2a178b4a6bc80e
Ankit Kumar <ankitks6688@gmail.com>
2026-09-13 12:03:30 +0530

    'java

--- src/Strings/Reversed_Words_in_a_Given_String.java
+++ src/Strings/Reversed_Words_in_a_Given_String.java
@@ -118,7 +118,9 @@ public class Reversed_Words_in_a_Given_String {
 	}
 	
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Enter String: ");
 	}
 
 }
