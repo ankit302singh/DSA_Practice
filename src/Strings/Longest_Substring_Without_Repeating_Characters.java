cffda3dc873a146a3113284c3d4d2df40efca700
Ankit Kumar <ankitks6688@gmail.com>
2026-09-22 22:48:52 +0530

    java

--- src/Strings/Longest_Substring_Without_Repeating_Characters.java
+++ src/Strings/Longest_Substring_Without_Repeating_Characters.java
@@ -1,4 +1,10 @@
 package Strings;
 
 public class Longest_Substring_Without_Repeating_Characters {
+    public static int longestSubstring(String s){
+        int maxLength = 0;
+        for(int i = 0; i < s.length(); i++){
+            
+        }
+    }
 }
