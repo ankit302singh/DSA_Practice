fb3746f1caa14c888a4f9d9859e5db4b8bfbbf3f
Ankit Kumar <ankitks6688@gmail.com>
2026-09-22 22:51:00 +0530

    java

--- src/Strings/Longest_Substring_Without_Repeating_Characters.java
+++ src/Strings/Longest_Substring_Without_Repeating_Characters.java
@@ -4,7 +4,11 @@ public class Longest_Substring_Without_Repeating_Characters {
     public static int longestSubstring(String s){
         int maxLength = 0;
         for(int i = 0; i < s.length(); i++){
-            
+            HashSet<Character> set = new HashSet<>();
+
+            for(int j = i; j < s.length(); j++){
+                
+            }
         }
     }
 }
