e3a8e0ae9b3e565cad3850cd0b7f752ba86926be
Ankit Kumar <ankitks6688@gmail.com>
2026-09-23 22:56:51 +0530

    java

--- src/Strings/Longest_Substring_Without_Repeating_Characters.java
+++ src/Strings/Longest_Substring_Without_Repeating_Characters.java
@@ -2,7 +2,7 @@ package Strings;
 import java.util.HashSet;
 
 public class Longest_Substring_Without_Repeating_Characters {
-    /*
+/*
     public static int longestSubstring(String s){
         int maxLength = 0;
         for(int i = 0; i < s.length(); i++){
@@ -26,10 +26,11 @@ public class Longest_Substring_Without_Repeating_Characters {
         System.out.println("longest substring: " + answer);
     }
 
-     */
+ */
 
     // << Better Way --> Using HashSet + Sliding Window >>
 
+    /*
     public static int longestSubstring(String s){
         int left = 0;
         int right = 0;
@@ -45,9 +46,9 @@ public class Longest_Substring_Without_Repeating_Characters {
                 set.remove(s.charAt(left));
                 left++;
             }
-            s.add(current);
+            set.add(current);
 
-            maxLength = Math.max(maxlength, right - left + 1);
+            maxLength = Math.max(maxLength, right - left + 1);
             right++;
         }
         return maxLength;
@@ -55,5 +56,12 @@ public class Longest_Substring_Without_Repeating_Characters {
     public static void main(String[] args){
         String s = "abcabcbb";
         int answer = longestSubstring(s);
+        System.out.println("longest Substring: " + answer);
     }
+
+     */
+
+    // << Optimal Way (Sliding window + Character Index>>
+
+    
 }
