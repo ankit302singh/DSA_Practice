eeb5320445eaea59a93277937bbbc689e1a12f24
Ankit Kumar <ankitks6688@gmail.com>
2026-09-24 23:31:37 +0530

    'java

--- src/Strings/Longest_Palindromic_Substring.java
+++ src/Strings/Longest_Palindromic_Substring.java
@@ -2,7 +2,14 @@ package Strings;
 
 public class Longest_Palindromic_Substring {
     public static boolean isPalindrome(String s){
+        int left = 0;
+        int right = s.length() - 1;
 
-        return  true; 
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false; 
+            }
+        }
+        return  true;
     }
 }
