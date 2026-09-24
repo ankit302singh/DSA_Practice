289f39d4ff82f5392b9df825bd0e6966039d92cd
Ankit Kumar <ankitks6688@gmail.com>
2026-09-24 23:29:32 +0530

    java

--- src/Strings/Longest_Palindromic_Substring.java
+++ src/Strings/Longest_Palindromic_Substring.java
@@ -1,25 +1,8 @@
 package Strings;
 
 public class Longest_Palindromic_Substring {
-    public String longestPandlindrome(String s){
-        String longest = "";
+    public static boolean isPalindrome(String s){
 
-        for(int i = 0; i < s.length(); i++){
-            for(int j = i; j < s.length(); j++){
-                String substring = s.substring(i, j+1);
-
-                if(isPalindrome(substring)){
-                    if(substring.length() > longest.length()){
-                        longest = substring;
-                    }
-                }
-            }
-        }
-        return longest;
+        return  true; 
     }
-    public boolean isPalindrome(String s ){
-
-        return true; 
-    }
-
 }
