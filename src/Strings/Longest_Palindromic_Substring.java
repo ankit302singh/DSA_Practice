b64633e7daac7a2b8fed2828cfa6bf9b7bf2b883
Ankit Kumar <ankitks6688@gmail.com>
2026-09-24 23:12:14 +0530

    java

--- src/Strings/Longest_Palindromic_Substring.java
+++ src/Strings/Longest_Palindromic_Substring.java
@@ -3,6 +3,12 @@ package Strings;
 public class Longest_Palindromic_Substring {
     public String longestPandlindrome(String s){
         String longest = "";
+
+        for(int i = 0; i < s.length(); i++){
+            for(int j = i; j < s.length(); j++){
+                
+            }
+        }
         return longest;
     }
 
